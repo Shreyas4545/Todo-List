ec25a0bdff13bb4e4eb5e8edd13e7d796d7b82e4
Shreyas4545 <shreyasjakati66@gmail.com>
2022-09-06 11:20:22 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,70 +1,116 @@
-//jshint esversion:6
+//jshint esversion:8
 const express=require("express");
 const https=require("https");
 // const request=require("request");
 const app=express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-var items=["Get up","Do workout","Go for work its workday !"];
 var b="";
-let workitems=[];
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const _=require("lodash");
+mongoose.connect("mongodb+srv://ShreyasJakati:20169361Shreyas@cluster0.m7dbtps.mongodb.net/todoListDB",{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended:true}));
-
+const listschema=new mongoose.Schema({
+name:String
+});
+const Item=new mongoose.model("Item",listschema);
+const ListSchema1=new mongoose.Schema({
+  name:String,
+  items:[listschema]
+});
+const List=new mongoose.model("List",ListSchema1);
+const item1=new Item({
+  name:"Welcome"
+});
+const item2=new Item({
+  name:"Get started"
+});
+const item3=new Item({
+  name:"<-- Press to delete this one !"
+});
+const itemss=[item1,item2,item3];
 app.get("/",function(req,res){
-  var a=new Date();
-  var options={
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-  };
-  // var currentdate=a.getDay();
-  // switch (currentdate) {
-  //     case 0:
-  //     x="Sunday";
-  //     break;
-  //     case 1:
-  //     x="Monday";
-  //     break;
-  //     case 2:
-  //     x="Tuesday";
-  //     break;
-  //     case 3:
-  //     x="Wednesday";
-  //     break;
-  //     case 4:
-  //     x="Thursday";
-  //     break;
-  //     case 5:
-  //     x="Friday";
-  //     break;
-  //     case 6:
-  //     x="Saturday";
-  //     break;
-  //     default:
-  //     console.log("Error ! The day is " + x);
-
-  var day=a.toLocaleDateString("en-US",options);
-  res.render('list', {ListTitle: day,newitem: items});
+  Item.find({},function(err,founditems){
+    if(founditems.length===0)
+    {
+      Item.insertMany(itemss,function(err){
+        if(err) console.log(err);
+        else console.log("Saved Successfully");
+      });
+      res.redirect("/");
+    }
+    else{
+    res.render('list', {ListTitle:"Today",newitem: founditems});
+  }
+  });
 });
 app.post("/",function(req,res){
-  // console.log(req.body);
 let b=req.body.newone;
-  if(req.body.List==="Work"){
-    workitems.push(b);
-    res.redirect("/work");
+let c=req.body.List;
+const a=new Item({
+  name:b
+});
+if(c==="Today")
+{
+  a.save();
+  res.redirect("/");
+}
+else{
+  List.findOne({name:c},function(err,foundlist){
+    foundlist.items.push(a);
+    foundlist.save();
+    res.redirect("/"+c);
+  });
+}
+});
+app.post("/delete",function(req,res){
+  const a=req.body.checkbox;
+  const b=req.body.ListName;
+  if(b==='Today')
+  {
+  Item.findByIdAndRemove(a,function(err){
+    if(!err) {
+        res.redirect("/");
+    }
+  });
+}
+else{
+  List.findOneAndUpdate({name:b},{$pull:{items:{_id:a}}},function(err,foundlist){
+    res.redirect("/"+b);
+  });
+}
+});
+app.get('/:dirname', function(req , res){
+  let a=req.params.dirname;
+//   function jsUcfirst(a)
+// {
+//     return a.charAt(0).toUpperCase() + a.slice(1);
+// }
+// a=jsUcfirst(a);
+a=_.capitalize(a);
+  List.findOne({name:a},function(err,founditem){
+    if(!err){
+    if(!founditem){
+      const itemlist=new List({
+        name:a,
+        items:itemss
+      });
+      itemlist.save();
+      res.redirect("/"+a);
+    }
+    else{
+      res.render("list",{ListTitle:founditem.name,newitem: founditem.items});
+    }
   }
-  else{
-    items.push(b);
-     res.redirect("/");
-  }
+  });
 });
-app.get("/work",function(req,res){
-  res.render("list",{ListTitle:"Work List",newitem:workitems});
-});
-app.get("/about",function(req,res){
-   res.render("about");
-});
+// document.querySelector("#abcd").click(function() {
+//       document.querySelector("#theform").submit();
+// });
 app.listen(process.env.PORT || 3000,function(){
   console.log("hello");
 });
+
+
+// mongosh "mongodb+srv://cluster0.m7dbtps.mongodb.net/myFirstDatabase" --apiVersion 1 --username ShreyasJakati
